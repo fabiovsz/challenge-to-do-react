600ab17da60a3d88972bc311a1870631c6af6ef4
fabiovsz <fabio.v2001@gmail.com>
2022-08-19 09:49:34 -0300

    edit styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import Header from "./components/Header";
-import TaskInput from "./components/TaskInput";
-import styles from "./App.module.css";
 import "./global.css";
 import Tasks from "./components/Tasks";
-import { useState } from "react";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className={styles.wrapper}>
-        <Tasks />
-      </div>
+      <Tasks />
     </div>
   );
 }
